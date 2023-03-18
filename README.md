64b8c0cb07066457c2766e1139230b25303ae1f5
Konstantin <konstantin.androsov@gmail.com>
2023-03-18 22:05:40 +0100

    First version
    Code based on
    https://github.com/hh-italian-group/AnalysisTools/tree/master/Instruments/source

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # SyncTool
+Tools to sync between two analysis frameworks
+
+## Installation
+
+```sh
+git clone https://github.com/cms-hh-bbtautau/SyncTool.git
+```
+
+## Environment
+
+Should work in a python environment with ROOT. For example, on lxplus, one can use the LCG environment:
+```sh
+# lxplus 7
+source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_102 x86_64-centos7-gcc11-opt
+# lxplus 8
+source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_102 x86_64-centos8-gcc11-opt
+```
+
+## EventSync
+
+Synchronise on an event-by-event basis. Output contains:
+- number of events and unique events for each group
+- list of duplicated events (if any) for each group
+- number of common events
+- comparison between variables defined in the config:
+  - common events with discrepancies above the threshold are listed in the log
+  - plots in PDF format:
+    - 1D plot with a comparison of distributions for all events
+    - 1D plot with a comparison of distributions for the common events
+    - 1D plot with a comparison of distributions for the different events
+    - 2D distribution for the common events: x=var_group2, y=(var_group2-var_group1)/var_group2
+
+How to run
+```sh
+python EventSync.py --config MY_CONFIG --channel MY_CHANNEL --sample MY_SAMPLE --group GROUP1 --file FILE1.ROOT --tree TREE1 --group GROUP2 --file FILE2.root --tree TREE2 &> sync.log
+```
+
+Example:
+```sh
+python EventSync.py --config config/PI_LLR.cfg --channel tauTau --sample TT --group PI --file TT_PI.root --tree Events --group LLR --file TT_LLR.root --tree HTauTauTree &> sync.log
+```
+
+## ShapeSync
+
+Synchronise on output shapes used for the limit extraction.
+
+Coming soon (code porting is in progress)
---
+++ include/SyncPlotsConfig.h
@@ -0,0 +1,224 @@
+/*! Configuration EventSync. */
+
+#pragma once
+
+#include "EnumNameMap.h"
+#include "NumericPrimitives.h"
+
+namespace analysis {
+
+enum class CondExpr { Less, More, Equal, LessOrEqual, MoreOrEqual };
+ENUM_NAMES(CondExpr) = {
+    { CondExpr::Less, "<" }, { CondExpr::More, ">" }, { CondExpr::Equal, "==" },
+    { CondExpr::LessOrEqual, "<=" }, { CondExpr::MoreOrEqual, ">=" },
+};
+
+struct Condition {
+    bool always_true;
+    std::string entry;
+    CondExpr expr;
+    bool is_integer;
+    int value_int;
+    double value_double;
+    Condition() : always_true(true), is_integer(true) {}
+
+    bool pass_int(int value) const { return pass_ex(value, value_int); }
+    bool pass_double(double value) const { return pass_ex(value, value_double); }
+
+private:
+    template<typename T>
+    bool pass_ex(T value, T cut_value) const
+    {
+        if(always_true) return true;
+        if(expr == CondExpr::Less) return value < cut_value;
+        if(expr == CondExpr::More) return value > cut_value;
+        if(expr == CondExpr::Equal) return value == cut_value;
+        if(expr == CondExpr::LessOrEqual) return value <= cut_value;
+        return value >= cut_value;
+    }
+};
+
+inline std::ostream& operator <<(std::ostream& s, const Condition& cond)
+{
+    if(cond.always_true)
+        s << 1;
+    else {
+        s << cond.entry << cond.expr;
+        if(cond.is_integer)
+            s << cond.value_int;
+        else
+            s << cond.value_double;
+    }
+    return s;
+}
+
+inline std::istream& operator >>(std::istream& s, Condition& cond)
+{
+    static const std::set<char> expr_chars = { '<', '>', '=' };
+    static const std::set<char> float_chars = { '.' };
+    static const std::set<char> stop_chars = { ' ', '\t' };
+
+    bool first = true;
+    cond.entry = "";
+    char c = '\0';
+    while(s.good()) {
+        s >> c;
+        if(first && c == '1') {
+            cond.always_true = true;
+            return s;
+        } else
+            cond.always_true = false;
+        first = false;
+        if(expr_chars.count(c)) break;
+        cond.entry.push_back(c);
+    }
+
+    std::stringstream ss_expr;
+    std::stringstream ss_value;
+    cond.is_integer = true;
+    ss_expr << c;
+    s >> c;
+    if(expr_chars.count(c))
+        ss_expr << c;
+    else {
+        cond.is_integer &= !float_chars.count(c);
+        ss_value << c;
+    }
+    ss_expr >> cond.expr;
+
+    while(s.good()) {
+        s >> c;
+        if(stop_chars.count(c)) break;
+        cond.is_integer &= !float_chars.count(c);
+        if(!s.fail())
+            ss_value << c;
+    }
+
+    if(cond.is_integer)
+        ss_value >> cond.value_int;
+    else
+        ss_value >> cond.value_double;
+
+    return s;
+}
+
+struct SyncPlotEntry {
+    static constexpr size_t N = 2;
+
+    std::array<std::string, N> names;
+    size_t n_bins;
+    Range<double> x_range;
+    std::array<Condition, N> conditions;
+
+    bool HasAtLeastOneCondition() const
+    {
+        for(size_t n = 0; n < N; ++n) {
+            if(!conditions[n].always_true)
+                return true;
+        }
+        return false;
+    }
+    SyncPlotEntry() : n_bins(0) {}
+};
+
+inline std::ostream& operator <<(std::ostream& s, const SyncPlotEntry& entry)
+{
+    static constexpr char sep = ' ';
+    for(size_t n = 0; n < SyncPlotEntry::N; ++n) {
+        if(entry.names[n].size())
+            s << entry.names[n] <<  sep;
+    }
+    s << entry.n_bins << sep << entry.x_range;
+    if(entry.HasAtLeastOneCondition()) {
+        for(size_t n = 0; n < SyncPlotEntry::N; ++n)
+            s << sep << entry.conditions[n];
+    }
+    return s;
+}
+
+inline std::istream& operator >>(std::istream& s, SyncPlotEntry& entry)
+{
+    std::string line;
+    std::getline(s, line);
+    const auto params = SplitValueList(line, true, " \t");
+
+    try {
+        if(params.size() >= 4 && params.size() <= 7) {
+            size_t n = 0;
+            entry.names[0] = params.at(n++);
+
+            if(TryParse(params.at(n), entry.n_bins)) {
+                entry.names[1] = entry.names[0];
+                ++n;
+            } else {
+                entry.names[1] = params.at(n++);
+                entry.n_bins = Parse<size_t>(params.at(n++));
+            }
+            const std::string range_str = params.at(n) + " " + params.at(n+1);
+            entry.x_range = Parse<Range<double>>(range_str);
+            n += 2;
+            if(n < params.size()) {
+                std::istringstream ss(params.at(n++));
+                ss >> entry.conditions[0];
+            }
+            if(n < params.size()) {
+                std::istringstream ss(params.at(n++));
+                ss >> entry.conditions[1];
+            }
+
+            return s;
+        }
+    } catch(exception& e) { std::cerr << "ERROR: " << e.what() << std::endl; }
+
+    throw exception("Invalid plot entry '%1%'.") % line;
+}
+
+
+class SyncPlotConfig {
+public:
+    static constexpr size_t N = SyncPlotEntry::N;
+
+    explicit SyncPlotConfig(const std::string& file_name)
+    {
+        std::ifstream cfg(file_name);
+
+        for(size_t n = 0; n < N; ++n)
+            idBranches[n] = ReadIdBranches(cfg);
+
+        while(cfg.good()) {
+            std::string cfgLine;
+            std::getline(cfg, cfgLine);
+            if (!cfgLine.size() || (cfgLine.size() && cfgLine.at(0) == '#')) continue;
+            SyncPlotEntry entry;
+            std::istringstream ss(cfgLine);
+            ss >> entry;
+            entries.push_back(entry);
+        }
+    }
+
+    const std::vector<std::string>& GetIdBranches(size_t n) const
+    {
+        if(n >= N)
+            throw exception("Invalid id branches index.");
+        return idBranches[n];
+    }
+
+    const std::vector<SyncPlotEntry>& GetEntries() const { return entries; }
+
+private:
+    std::vector<std::string> ReadIdBranches(std::ifstream& cfg)
+    {
+        std::string cfgLine;
+        std::getline(cfg, cfgLine);
+        auto idBranches = SplitValueList(cfgLine, false, " \t");
+        if(idBranches.size() < 3 || idBranches.size() > 4)
+            throw exception("Invalid event id branches line '%1%'.") % cfgLine;
+        return idBranches;
+    }
+
+private:
+    std::array<std::vector<std::string>, N> idBranches;
+    std::vector<SyncPlotEntry> entries;
+};
+
+} // namespace analysis
